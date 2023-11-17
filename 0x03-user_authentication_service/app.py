0001f4b930b5cc1e18a3165d68f5ccc81e7a8438
Adepoju Lateef <badepojulateef@gmail.com>
2023-11-17 21:49:01 +0100

    updated app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -108,6 +108,7 @@ def get_reset_password_token() -> str:
     except Exception:
         abort(403)
 
+
 @app.route('/reset_password', methods=['PUT'])
 def update_password() -> str:
     """Reset password endpoint."""
