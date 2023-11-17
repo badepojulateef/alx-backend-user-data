12c993d97f516cc94e4bb762ed3ab38b50d9b85c
Adepoju Lateef <badepojulateef@gmail.com>
2023-11-17 02:44:23 +0100

    added task 5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -42,3 +42,41 @@ def _generate_uuid() -> str:
         return str(id)
     except ValueError as e:
         raise ValueError("Error generating UUID.") from e
+
+
+class Auth:
+    """
+    Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """
+        Initialize a new Auth instance with a connection to
+        the authentication database.
+        """
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> Union[None, User]:
+        """
+        Register a new user in the authentication database.
+
+        Args:
+            email (str): The email of the user to be registered.
+            password (str): The password of the user.
+
+        Returns:
+            Union[None, User]: Returns None if the registration is
+            successful, otherwise raises a ValueError.
+
+        Raises:
+            ValueError: If the user with the given email already exists.
+        """
+        try:
+            # Attempt to find the user with the given email
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            # If user does not exist, add user to the database
+            return self._db.add_user(email, _hash_password(password))
+        else:
+            # If user already exists, raise an error
+            raise ValueError('User {} already exists'.format(email))
