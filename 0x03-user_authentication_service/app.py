5f88fb3475e1c7354ffa45ce1e4cc92625d3b86a
Adepoju Lateef <badepojulateef@gmail.com>
2023-11-17 03:50:33 +0100

    updated task 7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -46,5 +46,32 @@ def users() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route('/sessions', methods=['POST'])
+def login() -> str:
+    """
+    Check if the provided login credentials are valid.
+
+    Args:
+        email (str): The email of the user.
+        password (str): The password to check.
+
+    Returns:
+        bool: True if the login is valid, False otherwise.
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not (AUTH.valid_login(email, password)):
+        abort(401)
+
+    else:
+        # create a new session
+        session_id = AUTH.create_session(email)
+        res = jsonify({"email": email, "message": "logged in"})
+        res.set_cookie('session_id', session_id)
+
+    return res
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
